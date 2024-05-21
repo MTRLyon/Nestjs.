5db1ad0acbfd6e78d50a0825dcb8cee9136e1295
MTRLyon <jairo.m.m.2004@gmail.com>
2024-05-21 00:59:10 -0500

    package-lock.json

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -2,12 +2,17 @@ import { Controller, Delete, Patch, Get, Post, Put, Body, Query, Param} from "@n
 import { Taskservice } from "./tasks.service";
 import { CreateTaskDto } from "./dto/create-task-dto";
 import { UpdateTaskDto } from "./dto/update-task.dto";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 @Controller ('/tasks')
+@ApiTags('tasks')
 export class Taskcontroller {
     constructor(private tasksService:Taskservice) {}
 
     @Get()
+    @ApiOperation({ summary: 'Get all tasks'})
+    @ApiResponse({ status: 200, description: 'Return all tasks.' })
+    @ApiResponse({ status: 403, description: 'Forbidden'})
     getAllTasks(@Query() query: any) {
         console.log(query)
        return this. tasksService.getTasks();
@@ -19,7 +24,8 @@ export class Taskcontroller {
       return this. tasksService.getTask(parseInt(id));
     }
 
-    @Post()     
+    @Post()  
+    @ApiOperation({ summary: 'Create a task' })   
     CreateTask(@Body() task: CreateTaskDto) {
        return this.tasksService.createTask(task);
     }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -14,8 +14,4 @@ export class CreateUserDto {
     @IsString()
     @IsNotEmpty()
     name: string 
-
-    @IsNumber()
-    @Max(100)
-    age: number
 }
